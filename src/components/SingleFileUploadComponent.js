84f779949c7c6d575384ea40e5e76bdceb7b3165
yu-xiu <xiuyutiffany@gmail.com>
2024-05-03 13:40:45 -0700

    update ip address

--- src/components/SingleFileUploadComponent.js
+++ src/components/SingleFileUploadComponent.js
@@ -19,7 +19,7 @@ function SingleFileUploadComponent(props) {
     console.log(username);
     setResult("Processing Upload")
     //const response = await fetch('http://127.0.0.1/transcribe', {
-    const response = await fetch('https://23.22.153.179/transcribe', {
+    const response = await fetch('https://100.24.62.128/transcribe', {
         method: 'POST', // or 'PUT'
         headers: {
           'Content-Type': 'application/json',
